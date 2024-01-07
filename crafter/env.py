7cab9b0d3d7ec62527e61e491bf7cebf394238b7
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-07 10:04:34 +0000

    wip

--- crafter/env.py
+++ crafter/env.py
@@ -118,7 +118,7 @@ class Env(BaseClass):
     return obs, reward, done, info
 
   def render(self, size=None):
-    size =  self._size
+    size =  self._size = 256
     # in original code use size, this is to make it work with surprise adapt code
     unit = size // self._view
     canvas = np.zeros(tuple(size) + (3,), np.uint8)
